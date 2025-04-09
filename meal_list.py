07fef64e0cfd48b51c760fa6b4865a2deaba1b5a
lizfly256 <lizpow256@gmail.com>
2025-04-08 21:03:20 -0500

    Create meal_list.py

---
+++ meal_list.py
@@ -0,0 +1,277 @@
+recipes: list[dict[str, str | dict[str, float]] | dict[str, str | dict[str, float | int]] | dict[str, str | dict[str, float | int]] | dict[str, str | dict[str, float]] | dict[str, str | dict[str, int]] | dict[str, str | dict[str, float | int]]] = [
+    {
+        'name': 'Monty Jack Burgers',
+        'ingredients': {'potato (oz)': 12, 'garlic clove': 2, 'yellow onion': 1, 'ground beef (#)': 0.6, 'shredded monty jack (oz)': 4, 'potato bun': 2, 'creamy horseradish sauce (tbsp)': 2},
+        'instructions': 'pg 1'
+    },
+    {
+        'name': 'Sweat Chili Pork Tacos',
+        'ingredients': {'lime': 1, 'garlic clove': 2, 'peanuts (oz)': 3, 'coleslaw mix (oz)': 6, 'sour cream (oz)': 2, 'ground pork (#)': 0.6, 'sweet thai chili sauce (oz)': 1, 'ponzu sauce (oz)': 2, 'cornstarch (tbsp)': 1, 'taco tortillas': 6},
+        'instructions': 'pg 2'
+    },
+    {
+        'name': 'Creamy Garlic Rigatoni Chicken Sausage',
+        'ingredients': {'garlic clove': 2, 'chicken sausage (#)': 0.6, 'rigatoni (oz)': 6, 'peas (oz)': 10, 'cream sauce base (oz)': 4, 'mushroom stock concentrate': 1, 'garlic powder (tbsp)': 1},
+        'instructions': 'pg 3'
+    },
+    {
+        'name': 'Apricot Dijon Chicken',
+        'ingredients': {'carrot (oz)': 12, 'side green': 1, 'shallot': 1, 'chicken breast (#)': 2, 'chicken stock concentrate': 1, 'dijon mustard (tbsp)': 1, 'apricot jam (tbsp)': 2},
+        'instructions': 'pg 4'
+    },
+    {
+        'name': 'Beef Cheddar Melt',
+        'ingredients': {'garlic pepper (tsp)': 0.24, 'scallion': 2, 'mushroom seasoning (tsp)': 2, 'shredded cheddar cheese (oz)': 1, 'yukon potato (oz)': 12, 'mirepoix stock concentrate (tsp)': 2, 'cream cheese (oz)': 1, 'steak strips (oz)': 10},
+        'instructions': 'pg 5'
+    },
+    {
+        'name': 'Peach-Glazed Pork Chops',
+        'ingredients': {'red pepper flakes (tsp)': 0.25, 'garlic salt (tsp)': 0.5, 'butter (tbsp)': 1, 'sour cream (oz)': 2, 'russet potato (oz)': 10, 'side green': 1, 'bbq spice rub (tsp)': 1, 'boneless pork chop': 2},
+        'instructions': 'pg 6'
+    },
+    {
+        'name': 'Creamy Tomato and Italian Sausage Penne',
+        'ingredients': {'peas (oz)': 10, 'red pepper flakes (tsp)': 0.25, 'cream cheese (oz)': 1, 'scallion': 2, 'goat cheese (oz)': 1, 'grated parmesan (oz)': 0.5, 'penne pasta (oz)': 5, 'flour (oz)': 0.25, 'marinara sauce (oz)': 4, 'italian pork sausage (#)': 0.75},
+        'instructions': 'pg 7'
+    },
+    {
+        'name': 'Crispy Black Bean Burritos',
+        'ingredients': {'roma tomato': 1, 'yellow onion': 1, 'tex-mex paste (oz)': 1, 'jasmine rice': 1, 'black bean can (oz)': 10, 'cream cheese (oz)': 1, 'burrito tortilla': 2, 'guacamole': 1},
+        'instructions': 'pg 8'
+    },
+    {
+        'name': 'Sweet Umami Beef Bowls',
+        'ingredients': {'ground beef (#)': 0.5, 'jasmine rice': 1, 'garlic clove': 3, 'carrot (oz)': 3, 'mini cucumber': 1, 'sriracha': 1, 'ponzu sauce (oz)': 4, 'lime': 1, 'yellow onion': 1, 'scallion': 1},
+        'instructions': 'pg 9'
+    },
+    {
+        'name': 'Hotel Butter Steak',
+        'ingredients': {'parsley bunch': 1, 'garlic clove': 1, 'potato (oz)': 12, 'sliced almonds (oz)': 1, 'ranch steak': 2, 'green beans (oz)': 12},
+        'instructions': 'pg 10'
+    },
+    {
+        'name': 'Spicy Tex-Mex Black Bean Enchiladas',
+        'ingredients': {'pablano': 1, 'scallion': 1, 'garlic clove': 1, 'roma tomato': 1, 'cornstarch (tbsp)': 1, 'tex-mex paste (oz)': 1, 'southwest spice': 1, 'black bean can (oz)': 10, 'taco tortillas': 6, 'shredded monty jack (oz)': 3},
+        'instructions': 'pg 11'
+    },
+    {
+        'name': 'Creamy Tomato  and Pork Sausage Linguine',
+        'ingredients': {'roma tomato': 1, 'garlic clove': 2, 'linguine': 1, 'italian pork sausage (#)': 0.5, 'italian seasoning': 1, 'cream cheese (oz)': 1, 'chicken stock concentrate': 1, 'cream sauce base (oz)': 4, 'ciabatta': 1},
+        'instructions': 'pg 12'
+    },
+    {
+        'name': 'Chipotle Pork Tacos',
+        'ingredients': {'chipotle pesto (tbsp)': 1, 'mirepoix stock concentrate (tsp)': 2, 'sour cream (oz)': 1, 'yellow onion': 0.5, 'pepper': 1, 'lime': 1, 'taco tortillas': 6, 'fajita seasoning (tsp)': 2, 'sliced pork (#)': 0.6},
+        'instructions': 'pg 13'
+    },
+    {
+        'name': 'Saucy Cherry-Balsamic Pork Chops',
+        'ingredients': {'potato (oz)': 12, 'green beans (oz)': 12, 'garlic clove': 1, 'pork chop': 2, 'balsamic vinegar (oz)': 1, 'cherry jam (tbsp)': 3, 'chicken stock concentrate': 1, 'soy sauce (oz)': 1},
+        'instructions': 'pg 14'
+    },
+    {
+        'name': 'Lasagna Style Beef and pork Bolognese Gemelli',
+        'ingredients': {'shredded mozzarella (oz)': 2, 'red pepper flakes (tsp)': 0.25, 'grated parmesan (oz)': 2, 'marinara sauce (oz)': 8, 'baby spinach (oz)': 5, 'bolognese meat sauce (oz)': 12, 'gemelli pasta (oz)': 6},
+        'instructions': 'pg 21'
+    },
+    {
+        'name': 'Creamy Pork Sausage and Couscous Soup',
+        'ingredients': {'garlic clove': 2, 'yellow onion': 1, 'celery stalk': 1, 'italian seasoning': 1, 'tomato paste (oz)': 2, 'chicken stock concentrate': 1, 'mushroom stock concentrate': 1, 'cream cheese (oz)': 1, 'ciabatta': 1, 'carrot (oz)': 3, 'italian pork sausage (#)': 0.4},
+        'instructions': '15'
+    },
+    {
+        'name': 'Creamy Tomato and Chicken Sausage Cavatappi',
+        'ingredients': {'red bell pepper': 1, 'garlic clove': 1, 'cavatappi pasta (oz)': 1, 'chicken sausage (#)': 0.5, 'tomato paste (oz)': 1, 'garlic powder (tbsp)': 1, 'cream sauce base (oz)': 4},
+        'instructions': 'pg 16'
+    },
+    {
+        'name': 'Chicken Parm',
+        'ingredients': {'side green': 1, 'garlic clove': 1, 'italian seasoning': 1, 'panko (c)': 0.25, 'ciabatta': 1, 'chicken breast (#)': 0.6, 'sour cream (oz)': 1, 'marinara sauce (oz)': 3, 'shredded monty jack (oz)': 2},
+        'instructions': 'pg 17'
+    },
+    {
+        'name': 'Black Bean Enchilada',
+        'ingredients': {'enchilada sauce (oz)': 5, 'corn (oz)': 5, 'black bean can (oz)': 15, 'jalapeno': 1, 'red onion': 1, 'pablano': 1, 'taco seasoning (tsp)': 2, 'taco tortillas': 6, 'shredded white cheddar cheese (oz)': 2, 'sour cream (oz)': 2},
+        'instructions': 'pg 18'
+    },
+    {
+        'name': 'tuna fish casserole',
+        'ingredients': {'cream of chicken soup (can)': 1, 'tuna in water canned': 1, 'peas (oz)': 10, 'egg noodles': 1},
+        'instructions': 'R'
+    },
+    {
+        'name': 'Turkey Shepherds Pie',
+        'ingredients': {'potato (oz)': 12, 'garlic clove': 2, 'carrot (oz)': 3, 'celery stalk': 1, 'yellow onion': 1, 'dried thyme': 1, 'chicken stock concentrate': 1, 'sour cream (oz)': 1},
+        'instructions': 'pg 19'
+    },
+    {
+        'name': 'Pulled Chicken Salsa Verde Taquitos',
+        'ingredients': {'pablano': 1, 'taco seasoning (tsp)': 1, 'salsa verde (oz)': 12, 'scallion': 2, 'taco tortillas': 6, 'sour cream (oz)': 2, 'shredded mexican cheese blend (oz)': 2, 'cream cheese (oz)': 1},
+        'instructions': 'pg 20'
+    },
+    {
+        'name': 'Creamy Basil Pesto Chicken Pasta',
+        'ingredients': {'basil pesto (oz)': 2, 'red pepper flakes (tsp)': 0.25, 'cream cheese (oz)': 1, 'chicken stock concentrate': 1, 'grated parmesan (oz)': 2, 'cooked chicken breast (oz)': 16, 'cream sauce base (oz)': 4, 'grape tomatoes (oz)': 4, 'penne pasta (oz)': 5},
+        'instructions': 'pg 23'
+    },
+    {
+        'name': 'Smothered & Stuffed Meatloaves',
+        'ingredients': {'potato (oz)': 10, 'green bell pepper': 1, 'yellow onion': 1, 'fry seasoning (tsp)': 3, 'bread (slice)': 1, 'ground beef (#)': 0.5, 'shredded pepper jack (oz)': 2, 'dijon mustard (tbsp)': 1},
+        'instructions': 'pg 24'
+    },
+    {
+        'name': 'Southwest Pork Flautas',
+        'ingredients': {'roma tomato': 1, 'yellow onion': 1, 'lime': 1, 'garlic clove': 2, 'sour cream (oz)': 1, 'ground pork (#)': 0.7, 'southwest spice': 1, 'chicken stock concentrate': 1, 'taco tortillas': 6, 'shredded monty jack (oz)': 2},
+        'instructions': 'pg 25'
+    },
+    {
+        'name': 'Creamy Corn & Bacon Chowder',
+        'ingredients': {'yellow onion': 1, 'garlic clove': 2, 'potato (oz)': 12, 'jalapeno': 1, 'corn (oz)': 10, 'chicken stock concentrate': 1, 'cream cheese (oz)': 2, 'sour cream (oz)': 4},
+        'instructions': 'pg 26'
+    },
+    {
+        'name': 'Kung Pao Bowls',
+        'ingredients': {'jasmine rice': 1, 'green bell pepper': 1, 'ginger (nub)': 1, 'scallion': 1, 'garlic clove': 1, 'peanuts (oz)': 1, 'ground beef (#)': 0.5, 'sweet soy glaze (oz)': 2},
+        'instructions': 'pg 27'
+    },
+    {
+        'name': 'Cowboy Fries Chicken Wraps',
+        'ingredients': {'potato (oz)': 10, 'yellow onion': 1, 'roma tomato': 1, 'southwest spice': 1, 'chicken breast (#)': 0.7, 'burrito tortilla': 2},
+        'instructions': 'pg 28'
+    },
+    {
+        'name': "General Tso's Pork Meatballs",
+        'ingredients': {'chili garlic sauce (tsp)': 1, 'panko (c)': 0.13, 'toasted sesame oil (oz)': 0.25, 'sweet and sour sauce': 1, 'garlic sesame sauce (oz)': 1, 'peanuts (oz)': 0.5, 'green beans (oz)': 12, 'asian garlic, ginger, & chile seasoning (tsp)': 1, 'ground pork (#)': 0.7},
+        'instructions': 'pg 29'
+    },
+    {
+        'name': 'Classic Chicken Cutlet Piccata',
+        'ingredients': {'spaghetti (oz)': 5, 'lemon': 1, 'flour (oz)': 1, 'grated parmesan (oz)': 0.5, 'chicken cutlet (#)': 0.7, 'roasted garlic and herb butter (oz)': 0.75, 'capers (oz)': 0.5, 'butter (oz)': 0.6, 'garlic pepper (tsp)': 2, 'shallot': 1},
+        'instructions': 'pg 30'
+    },
+    {
+        'name': 'Chile-Lime Sliced Steak Rice Bowl',
+        'ingredients': {'chile and cumin rub (tsp)': 1, 'jasmine rice': 1, 'jalapeno': 1, 'lime': 1, 'adobo seasoning (tsp)': 1, 'chile lime butter (oz)': 0.8, 'shallot': 1, 'pineapple chunks (oz)': 2, 'corn (oz)': 3, 'steak strips (#)': 0.7},
+        'instructions': 'pg 31'
+    },
+    {
+        'name': 'Italian Pork Sausage and Pepper Subs',
+        'ingredients': {'yukon gold potato (oz)': 10, 'italian pork sausage (#)': 0.5, 'red bell pepper': 1, 'garlic powder (tbsp)': 0.5, 'dijon mustard (tbsp)': 1, 'ketchup': 1, 'italian seasoning': 1, 'yellow onion': 1, 'demi-baguette': 2, 'chicken stock concentrate': 1, 'shredded mozzarella (oz)': 3},
+        'instructions': 'pg 32'
+    },
+    {
+        'name': 'Corn and Steak Strip Risotto',
+        'ingredients': {'corn (oz)': 3, 'scallion': 2, 'roma tomato': 1, 'pot-roast seasoning (tsp)': 1.5, 'arborio rice (oz)': 3.6, 'grated parmesan (oz)': 2, 'sour cream': 2, 'beef demi-glace (tsp)': 2, 'steak strips (#)': 0.7},
+        'instructions': 'pg 33'
+    },
+    {
+        'name': 'Hearty Chicken Sausage & Kale Soup',
+        'ingredients': {'cream cheese (oz)': 1.5, 'yellow onion': 1, 'garlic clove': 2, 'potato (oz)': 12, 'kale': 1, 'chicken sausage (#)': 0.5, 'chicken stock concentrate': 1, 'italian seasoning': 1, 'sour cream (oz)': 2},
+        'instructions': 'pg 34'
+    },
+    {
+        'name': 'Sliced Pork Lo Mein',
+        'ingredients': {'shredded red cabbage (oz)': 3, 'scallion': 2, 'cornstarch (tbsp)': 0.25, 'soy sauce (oz)': 0.2, 'brown stir fry sauce': 4, 'asian noodles (oz)': 10, 'sliced pork (#)': 0.7, 'sesame seeds (tsp)': 1, 'hot sauce (oz)': 0.25},
+        'instructions': 'pg 35'
+    },
+    {
+        'name': 'Cheesy Smokehouse Beef Hash',
+        'ingredients': {'potato (oz)': 12, 'pablano': 1, 'red onion': 1, 'lime':1, 'bbq sauce (oz)': 2, 'sour cream (oz)': 1, 'ground beef (#)': 0.5, 'shredded cheddar cheese (oz)': 1},
+        'instructions': 'pg 36'
+    },
+    {
+        'name': 'Italian Pork Sausage Marinara Subs',
+        'ingredients': {'potato': 10, 'yellow onion': 1, 'garlic clove': 1, 'demi-bagguette': 2, 'italian pork sausage (#)': 0.4, 'marinara sauce (oz)':6, 'shredded monty jack (oz)': 3},
+        'instructions': 'pg 37'
+    },
+    {
+        'name': 'Hot Honey Chicken',
+        'ingredients': {'yukon gold potato (oz)': 10, 'panko (c)': 0.5, 'sweet and smokey bbq seasoning (tbsp)': 1, 'chicken breast (oz)': 12, 'sour cream (oz)': 2, 'side green': 1, 'hot honey (oz)': 0.75},
+        'instructions': 'pg 38'
+    },
+    {
+        'name': "Southwest Queso Mac 'N' Cheese",
+        'ingredients': {'scallion': 2, 'tex-mex paste (oz)': 1, 'queso blanco (oz)': 3, 'cream cheese (tbsp)': 2, 'cornstarch (tbsp)': 1, 'mexican cheese blend (oz)': 2, 'cavatappi pasta (oz)': 6, 'shredded monty jack (oz)': 6, 'panko (c)': 0.5},
+        'instructions': 'pg 39'
+    },
+    {
+        'name': 'Cheesy Southwest Beef Tacodillas',
+        'ingredients': {'roma tomato': 1, 'yellow onion': 1, 'lime': 1, 'garlic cloves': 2, 'ground beef (#)': 0.5, 'southwest spice blend': 1, 'beef stock concentrate': 1, 'taco tortillas': 6, 'shredded mozzarella (oz)': 3},
+        'instructions': 'pg 40'
+    },
+    {
+        'name': 'Balsamic Frig Chicken',
+        'ingredients': {'yukon gold potato (oz)': 12, 'shallot': 1, 'rosemary': 1, 'lemon': 1, 'side green': 1, 'chicken breast (#)': 0.7, 'balsamic vinegar (oz)': 2, 'fig jam (tbsp)': 2, 'chicken stock concentrate': 1},
+        'instructions': 'pg 41'
+    },
+    {
+        'name': "Sweet 'N' Smokey Pork Tenderloin",
+        'ingredients': {'scallion': 1, 'white wine vinegar (oz)': 1, 'yukon gold potato (oz)': 12, 'smokey cinnamon paprika spice (tbsp)': 1, 'cherry jam (oz)': 1, 'carrot, shredded (oz)': 4, 'pork tenderloin (#)': 0.8, 'apple': 1, 'sour cream (oz)': 1},
+        'instructions': 'pg 42'
+    },
+    {
+        'name': 'Chicken Shawarma Bowl',
+        'ingredients': {'garlic clove': 2, 'yogurt (tbsp)': 4, 'shawarma spice blend (tbsp)': 1, 'chicken cutlet (#)': 0.7, 'zucchini': 1, 'roma tomato': 1, 'scallion': 2, 'lemon': 1, 'sour cream (oz)': 2, 'chicken stock concentrate': 1, 'turmeric (tsp)': 1, 'couscous (c)': 0.5, 'hummus (oz)': 2, 'hot sauce (tsp)': 1},
+        'instructions': 'pg 43'
+    },
+    {
+        'name': 'Korean-Style Pork Tacos',
+        'ingredients': {'lime': 1, 'scallion': 2, 'mayo (oz)': 0.8, 'hot sauce (oz)': 0.5, 'slaw mix (oz)': 4, 'gochujang red pepper paste (tbsp)': 1, 'ginger (nub)':1, 'taco tortilla': 6},
+        'instructions': 'pg 44'
+    },
+    {
+        'name': 'Chicken Sausage & Ravioli',
+        'ingredients': {'scallion': 2, 'roma tomato': 1, 'lemon': 1, 'italian chicken sausage (#)': 0.5, 'italian seasoning': 1, 'spinach and cheese ravioli (oz)': 9, 'chicken stock concentrate': 1, 'sour cream (oz)': 2},
+        'instructions': 'pg 45'
+    },
+    {
+        'name': 'Caramelized Onion Burger',
+        'ingredients': {'potato (oz)': 10, 'yellow onion': 1, 'garlic clove': 1, 'potato bun': 2, 'ground beef (#)': 0.6, 'dijon mustard (tbsp)': 2},
+        'instructions': '46'
+    },
+    {
+        'name': 'Ground Turkey Quesadilla',
+        'ingredients': {'taco tortilla': 6, 'chipotle crema (oz)': 2, 'shredded mozzarella (oz)': 4, 'red onion': 0.5, 'taco seasoning (tbsp)': 1, 'cilantro (oz)': 0.25, 'lime': 1, 'jalapeno': 1, 'ground turkey (#)': 0.6},
+        'instructions': 'pg 47'
+    },
+    {
+        'name': 'Creamy Cajun-Style Italian Sausage Gemelli',
+        'ingredients': {'gemelli pasta (oz)': 6, 'pablano': 1, 'roma tomato': 1, 'grated parmesan (oz)': 1, 'scallion': 2, 'cream sauce base (oz)': 4, 'cajun seasoning (tsp)': 2, 'italian pork sausage (#)': 0.5},
+        'instructions': 'pg 48'
+    },
+    {
+        'name': 'Sweet Chili Chicken',
+        'ingredients': {'scallion': 1, 'garlic clove': 2, 'jasmine rice': 1, 'carrot (oz)': 12, 'lime': 1, 'soy sauce (oz)': 1, 'sweet thai chili sauce (oz)': 3, 'chicken breast (#)': 10},
+        'instructions': 'pg 49'
+    },
+    {
+        'name': 'Italian Sausage Fettuccine Arrabbiata',
+        'ingredients': {'crushed tomatoes (oz)': 4, 'fettuccine (oz)': 4, 'red pepper flakes (tsp)': 0.25, 'peas (oz)': 3, 'cream sauce base (oz)': 4, 'italian seasoning': 1, 'garlic clove': 2, 'italian pork sausage (#)': 0.5},
+        'instructions': 'pg 50'
+    },
+    {
+        'name': 'Smothered Pork Chops with Onion Gravy',
+        'ingredients': {'potato (oz)': 12, 'green beans (oz)': 12, 'yellow onion': 1, 'garlic clove': 2, 'pork chops (#)': 0.6, 'worcestershire sauce (oz)': 0.75},
+        'instructions': 'pg 51'
+    },
+    {
+        'name': 'Tex-Mex Marinated Pork Tacos',
+        'ingredients': {'garlic clove': 2, 'lime': 1, 'roma tomato': 1, 'yellow onion': 1, 'tex-mex paste': 1, 'sour cream (oz)': 2, 'pork chops (#)': 0.75, 'taco tortillas': 6},
+        'instructions': 'pg 22'
+    },
+]
+
+
+
+sections = {
+    'pantry': ['can', 'canned', 'rigatoni', 'noodle','capers', 'salsa', 'sriracha', 'concentrate', 'box', 'jar', 'powder', 'garlic pepper', 'mustard', 'spice', 'pepper flakes', 'pesto',  'guacamole', 'starch', 'vinegar', 'flour', ' oat ', 'paste', 'rice', 'tortilla', 'soup', 'stock', 'pantry', 'peanut', 'jam', 'sauce', 'seasoning', 'salt', 'panko', 'pasta'],
+    'bakery': ['bread', 'bun', 'loaf', 'roll', 'ciabatta'],
+    'dairy': ['butter', 'milk', 'cheese', 'cream', 'milk', 'mozzarella', 'parmesan', 'jack', 'cheddar'],
+    'meat': ['sausage', 'steak', 'meat', 'beef', 'chicken', 'turkey', 'pork', 'bacon', 'steak', 'brats'],
+    'produce': ['onion', 'potato', 'green', 'celery', 'pepper', 'kale', 'pablano', 'carrot', 'jalapeno', 'lettus', 'lime', 'coleslaw', 'tomato', 'cabbage', 'cucumber', 'shallot', 'scallion', 'spinach', 'clove'],
+}
+
+
+
+
+
