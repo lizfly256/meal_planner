d1d04591ed243e5309a4ebe0004c2de4b12cecde
lizfly256 <lizpow256@gmail.com>
2025-04-08 21:01:19 -0500

    Update main_grocery_project.py

--- main_grocery_project.py
+++ main_grocery_project.py
@@ -1,80 +1,120 @@
-from recipe_ingredients import get_ingredients
-from recipe_list import recipes
+from meal_list import recipes
+from meal_list import sections
 import random
-grocery_list = dict()
 
-def create_menue() :
-    """Retunrns a list of recipies"""
-    menue = list()
-    while True :
-        new_meal = input("Enter a meal: ").lower()
-        if new_meal == "done" : break
-        # Add check that item is available (object created for it)
-        menue.append(new_meal)
-    return menue
 
-    
-def random_menue(num_of_meals) :
+def random_menu(num_of_meals):
     """Returns a certain number of randomly selected meals"""
     i = 0
     num_list = list()
-    menue = list()
+    menu = list()
     groceries = {}
-    while len(menue) < num_of_meals :
+    while len(menu) < num_of_meals:
         num = random.randint(0, len(recipes) - 1)
-        if num in num_list : continue
+        if num in num_list: continue
         num_list.append(num)
-        # Adds meal to menue
+        # Adds meal to menu
         new_meal = recipes[num]['name']
-        menue.append(new_meal)
+        menu.append(new_meal)
         # Adds ingredients to grocery list
         new_ingredients = recipes[num]['ingredients']
-        for k, v in new_ingredients.items() :
+        for k, v in new_ingredients.items():
             q = float(v)
-            groceries[k] = groceries.get(k,0) + q
+            groceries[k] = groceries.get(k, 0) + q
         i += 1
-    return menue, groceries
-    
-
-def original() :
-    """Original attempt, returns a grocery list created as user inputs meal names"""
-    while True :
-        recipe = input('recipe to add: ').lower()
-        if recipe == 'done' : break
-        if len(recipe) < 1 : recipe = 'grilled cheese' ###Remove when done, for quicker testing
-        ingredients = get_ingredients(recipe)
-        if ingredients == False :
-            print('Recipe not found. Try again.\n')
-            continue
-        
-        #loop to add ingredients to grocery list.
-        for item in ingredients :
-            #print(item)
-            grocery_list[item] = grocery_list.get(item,0) + 1 #idium: retreive/create/update
-    return grocery_list
+    return menu, groceries
 
 
+def sort_groceries(groceries):
+    """ Returns a grocery list sorted by section of the store based on whole grocery list"""
+    # pantry, dairy, meat, produce, bakery, other
+    pantry = dict()
+    dairy = dict()
+    meat = dict()
+    produce = dict()
+    bakery = dict()
+    other = dict()
+    # Check each item
+    for k, v in groceries.items():
+        if check_pantry(k):
+            pantry[k] = pantry.get(k, 0) + v
+        elif check_bakery(k):
+            bakery[k] = bakery.get(k, 0) + v
+        elif check_dairy(k):
+            dairy[k] = dairy.get(k, 0) + v
+        elif check_meat(k):
+            meat[k] = meat.get(k, 0) + v
+        elif check_produce(k):
+            produce[k] = produce.get(k, 0) + v
+        else:
+            other[k] = other.get(k, 0) + v
+        # Combine sections back into sorted dict
+    sorted_list = {'PANTRY': pantry, 'DAIRY': dairy, 'MEAT': meat, 'PRODUCE': produce, 'BAKERY': bakery, 'OTHER': other}
+    return sorted_list
 
-def grocery_list(menue) :
-    """Retuns a grocery list from a menue, using recipe objects"""
 
-        
-        #locate the item on other file
-        #for each key in ingredients dict, .get(item,o) + 1
+def check_pantry(name):
+    """ Returns True if item is in produce section, otherwise False"""
+    for k in sections['pantry']:
+        if k in name:
+            return True
+    return False
 
 
+def check_dairy(name):
+    """ Returns True if item is in dairy section, otherwise False"""
+    for k in sections['dairy']:
+        if k in name:
+            return True
+    return False
 
 
-x = random_menue(3)
-menue = x[0]
+def check_meat(name):
+    """ Returns True if item is in meat section, otherwise False"""
+    for k in sections['meat']:
+        if k in name:
+            return True
+    return False
+
+def check_produce(name):
+    """ Returns True if item is in produce section, otherwise False"""
+    for k in sections['produce']:
+        if k in name:
+            return True
+    return False
+
+
+def check_bakery(name):
+    """Returns True is item is in bakery, otherwise False"""
+    for k in sections['bakery']:
+        if k in name:
+            return True
+        return False
+
+def itemize_ingredients():
+    """Returns an alphabetized list of the ingredient names used in the recipies...to make adding new ones easier"""
+    i=0
+    store = list()
+    while i < len(recipes):
+        new_ingredients = recipes[i]['ingredients']
+        for k in new_ingredients:
+            if k not in store:
+                store.append(k)
+        i += 1
+        store.sort()
+    for item in store:
+        print(item)
+
+
+x = random_menu(3)
+menu = x[0]
 groceries = x[1]
-print(menue)
-print(groceries)
-#print(original())
-#print(create_menue())
+print(menu)
+sorted_list= sort_groceries(groceries)
+print('PANTRY-- ', sorted_list['PANTRY'])
+print('DAIRY-- ', sorted_list['DAIRY'])
+print('MEAT-- ', sorted_list['MEAT'])
+print('PRODUCE-- ', sorted_list['PRODUCE'])
+print('OTHER-- ', sorted_list['OTHER'])
 
-
-
-
-### Future feature: Create seperate grocery lists based on section of the store (Dairy, Meat, Veggie, Pantry, Other)
-### (Optional?) Random selection of (4?) recipies rather than inputting your own
+#itemize_ingredients()
